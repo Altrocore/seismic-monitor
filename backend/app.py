3bbc0c56ab5397903fdadd2ef112c13ee5447cca
Altrocore <shepelev.daniel.en@gmail.com>
2024-04-20 00:25:58 -0230

    fix CORS, fix get_felt_reports api data extracting

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,25 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from emsc_services.seismic_events import get_seismic_events
 from emsc_services.felt_reports import get_felt_reports
 from usgs_services.volcano_reports import get_volcano_reports
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 
 @app.route('/')
+@cross_origin()
 def home():
     return "Welcome to the Seismic Activity Monitor API"
 
 @app.route('/seismic-events')
+@cross_origin()
 def seismic_events():
     data = get_seismic_events()
     return jsonify(data)
 
 @app.route('/felt-reports')
+@cross_origin()
 def felt_reports():
     data, status_code = get_felt_reports()
     if status_code != 200:
@@ -25,6 +28,7 @@ def felt_reports():
     return jsonify(data)  # If all is well, return the data
 
 @app.route('/volcano-reports')
+@cross_origin()
 def volcano_reports():
     data = get_volcano_reports()
     return jsonify(data)
--- backend/emsc_services/felt_reports.py
+++ backend/emsc_services/felt_reports.py
@@ -3,5 +3,10 @@ from flask import jsonify
 
 def get_felt_reports():
     url = "https://www.seismicportal.eu/testimonies-ws/api/search?&format=json&downloadAsFile=false&orderby=time-desc&offset=0&limit=10"
-    response = requests.get(url)
-    return response.json()
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return(response.json(), response.status_code)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None, 500
